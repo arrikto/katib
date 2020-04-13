40b5b51a56e03e22506d3c368622103171b10c73
Ilias Katsakioris <elikatsis@arrikto.com>
2020-04-14 01:09:03 +0300

    Experiment: Use all assignments when Suggestion is failed/exhausted

--- pkg/controller.v1alpha3/experiment/experiment_controller.go
+++ pkg/controller.v1alpha3/experiment/experiment_controller.go
@@ -378,16 +378,22 @@ func (r *ReconcileExperiment) ReconcileSuggestions(instance *experimentsv1alpha3
 	} else {
 		if original != nil {
 			activeTrials := instance.Status.TrialsPending + instance.Status.TrialsRunning
-			if original.IsFailed() && activeTrials == 0 {
-				msg := "Suggestion has failed"
+			// If Suggestion is in failed/exhausted state, wait for active trials and use all produced suggestions
+			if (original.IsFailed() || original.IsExhausted()) && activeTrials == 0 && len(original.Status.Suggestions) == int(currentCount) {
+				var msg string
+				if original.IsFailed() {
+					msg = "Suggestion has failed"
+				} else {
+					msg = "Suggestion is exhausted"
+				}
 				instance.MarkExperimentStatusFailed(util.ExperimentFailedReason, msg)
-			} else if !original.IsFailed() {
+			} else {
 				suggestion := original.DeepCopy()
 				if len(suggestion.Status.Suggestions) > int(currentCount) {
 					suggestions := suggestion.Status.Suggestions
 					assignments = suggestions[currentCount:]
 				}
-				if suggestion.Spec.Requests != suggestionRequestsCount {
+				if !original.IsCompleted() && suggestion.Spec.Requests != suggestionRequestsCount {
 					suggestion.Spec.Requests = suggestionRequestsCount
 					if err := r.UpdateSuggestion(suggestion, suggestionRequestsCount); err != nil {
 						return nil, err
